3cf479c0e424520afa1c56150983dbc99de28720
EJ-David <daviderhabor.de@gmail.com>
2023-07-03 00:26:37 +0100

    fixed broken login. 24/29 tests passing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TLenght from "./TLenght";
 
@@ -19,27 +19,36 @@ function App() {
   const [timerId, setTimerId] = useState(null); // Timer interval ID
   const [displayTime, setDisplayTime] = useState(sessionTime * 60); // Displayed time in seconds
 
+  useEffect(() => {
+    setDisplayTime(isBreak ? breakTime * 60 : sessionTime * 60);
+  }, [sessionTime, breakTime, isBreak]);
+
   const handleTimeChange = (e) => {
-    setIsRunning(false)
+    setIsRunning(false);
+    console.log("stopped");
+    clearInterval(timerId);
+    setTimerId(null);
+
     const { id, className } = e.target;
     const isIncrement = className.includes("up");
 
     if (id.includes("break")) {
       if (isIncrement && breakTime < 60) {
         setBreakTime((prev) => prev + 1);
-      } else if (!isIncrement && breakTime > 0) {
+      } else if (!isIncrement && breakTime > 1) {
         setBreakTime((prev) => prev - 1);
       }
     } else if (id.includes("session")) {
       if (isIncrement && sessionTime < 60) {
         setSessionTime((prev) => prev + 1);
-      } else if (!isIncrement && sessionTime > 0) {
+      } else if (!isIncrement && sessionTime > 1) {
         setSessionTime((prev) => prev - 1);
       }
     }
   };
 
   const handleReset = () => {
+    console.log("stopped");
     clearInterval(timerId);
     setBreakTime(5);
     setSessionTime(25);
@@ -50,29 +59,27 @@ function App() {
   };
 
   const handlePlay = () => {
-    if(!isRunning){
-      setIsRunning(prev => {
-        return !prev
-      })
-      console.log('runing')
-      setTimerId(() => {
-        setDisplayTime((prevTime) => {
-          if (prevTime <= 0) {
-            clearInterval(timerId);
-            setIsBreak((prevBreak) => !prevBreak);
-            return isBreak ? sessionTime * 60 : breakTime * 60;
-          } else {
-            return prevTime - 1;
-          }
-        });
-      }, 1000)
-    }else if(isRunning){
-      setIsRunning(prev => {
-        return !prev
-      })
-      console.log('paused')
-      // clearInterval(timerId);
-      // setTimerId(null);
+    if (!isRunning) {
+      setIsRunning(true);
+      console.log("runing");
+      setTimerId(
+        setInterval(() => {
+          setDisplayTime((prevTime) => {
+            if (prevTime <= 0) {
+              clearInterval(timerId);
+              setIsBreak((prevBreak) => !prevBreak);
+              return isBreak ? sessionTime * 60 : breakTime * 60;
+            } else {
+              return prevTime - 1;
+            }
+          });
+        }, 1000)
+      );
+    } else {
+      setIsRunning(false);
+      console.log("paused");
+      clearInterval(timerId);
+      setTimerId(null);
     }
   };
 
@@ -92,7 +99,7 @@ function App() {
         />
       </div>
       <div id="timer-label">
-        <h3>{isBreak ? 'Break' : 'Session'}</h3>
+        <h3>{isBreak ? "Break" : "Session"}</h3>
         <h4 id="time-left">{formatTime(displayTime)}</h4>
         <div id="mainBtn">
           <button id="start_stop" onClick={handlePlay}>
